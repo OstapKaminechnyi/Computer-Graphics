00c67e5ad31aea9b569cb80c8ee70c9c381e3e20
OstapKaminechnyi <ostap1599@gmail.com>
2019-10-06 13:55:23 +0300

    tanks 1v

---
+++ TANKS/Assets/Scripts/Wall/ShatterOnCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShatterOnCollision : MonoBehaviour {
+
+	public GameObject replacement;
+
+
+
+	void OnCollisionEnter(){
+		GameObject.Instantiate(replacement,transform.position,transform.rotation);
+		Destroy(gameObject);
+	}
+	
+}
